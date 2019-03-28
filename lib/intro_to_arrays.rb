d083b889df83c738bd50aa05e60b71c39d4c8afa
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-03-28 15:31:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -26,8 +26,9 @@ def first_element_with_array_methods(south_east_asia)
   
 end
 
-def last_element_with_array_methods
-  my_new_array.last 
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.last 
 end
 
 def length_of_array
