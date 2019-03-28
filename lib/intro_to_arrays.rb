8f02a41c1402256bee7cfff33d7c5f55567722bd
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-03-28 15:23:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  da_bros = ["Justin", "Joseph"]
+end
+
+def first_element(array)
+my_new_array = "Welcome to New York"
+end
+
+def third_element(array)
+  my_new_array = "Style"
+
+end
+
+def last_element(array)
+  my_new_array = "Out of The Woods"
+  
+end
+
+def first_element_with_array_methods
+  south_east_asia = 
+  
+end
+
+def last_element_with_array_methods
+  my_new_array.last 
+end
+
+def length_of_array
+  
+end
